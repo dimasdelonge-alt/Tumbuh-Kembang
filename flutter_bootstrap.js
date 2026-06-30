237ff4692ee5cb279e0862fe101527a99d21905b
dimasdelonge-alt <dimasdelonge-alt@users.noreply.github.com>
2026-06-30 03:25:39 +0000

    deploy: c044f1b8953cc360bcb53afdf812e424a2e0abdb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "262205743"
+    serviceWorkerVersion: "3123042456"
   }
 });
